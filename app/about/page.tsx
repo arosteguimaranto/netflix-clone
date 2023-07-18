d53d869f552436e05a9b026552ebcfea90503595
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-07-17 23:05:09 -0600

    Añadiendo Prisma

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Input from "@/components/input";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export default function Auth() {
 
@@ -10,21 +10,21 @@ export default function Auth() {
 
   const [variant, setvariant] = useState('login');
 
-  const toggleVariant = useCallBack(() => {
+  const toggleVariant = useCallback(() => {
     setvariant((currentVariant)=> currentVariant === 'login' ? 'register' : 'login');
 
   }, [])
   
 
   return (
-    <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className=" bg-black w-full h-full lg:bg-opacity-50">
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className=" px-12 py-5">
           <img src="/images/logo.png" alt="Logo" className="h-12" />
         </nav>
         <div className="flex justify-center">
           <div className=" bg-black bg-opacity-70 px-16 self-center mt-2 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
+            <h2 className="text-white text-4xl mt-8 mb-8 font-semibold">
               {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
             <div className="flex flex-col gap-4">
@@ -49,14 +49,13 @@ export default function Auth() {
               value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              Login
-            </button>
-            <p className=" text-neutral-500 mt-12">
-              First time using Netflix?
-              <span onClick={toggleVariant} className=" text-white ml-1 hover:underline cursor-pointer">
-                Create account
+           
+            <p className="text-neutral-500 mt-8 mb-8">
+              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                {variant === 'login' ? 'Create an account' : 'Login'}
               </span>
+              .
             </p>
           </div>
         </div>
