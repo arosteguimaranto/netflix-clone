20b6248188ad28c1dfdc98288736b65e47966bf2
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-07-18 15:28:25 -0600

    fix: login form

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,19 @@
-'use client'
+"use client";
 import Input from "@/components/input";
 import { useState, useCallback } from "react";
 
 export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-
-  const [variant, setvariant] = useState('login');
+  const [variant, setvariant] = useState("login");
 
   const toggleVariant = useCallback(() => {
-    setvariant((currentVariant)=> currentVariant === 'login' ? 'register' : 'login');
-
-  }, [])
-  
+    setvariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -25,35 +24,56 @@ export default function Auth() {
         <div className="flex justify-center">
           <div className=" bg-black bg-opacity-70 px-16 self-center mt-2 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mt-8 mb-8 font-semibold">
-              {variant === 'login' ? 'Sign in' : 'Register'}
+              {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e: any) => {
+                    setName(e.target.value);
+                  }}
+                  id="name"
+                  value={name}
+                />
+              )}
+
               <Input
-              label="Username"
-              onChange={(e: any) =>  {setName(e.target.value)}}
-              id="name"
-              value={name}
+                label="Email"
+                onChange={(e: any) => {
+                  setEmail(e.target.value);
+                }}
+                id="email"
+                type="email"
+                value={email}
               />
               <Input
-              label="Email"
-              onChange={(e: any) =>  {setEmail(e.target.value)}}
-              id="email"
-              type="email"
-              value={email}
-              />
-              <Input
-              label="Password"
-              onChange={(e: any) =>  {setPassword(e.target.value)}}
-              id="password"
-              type="password"
-              value={password}
+                label="Password"
+                onChange={(e: any) => {
+                  setPassword(e.target.value);
+                }}
+                id="password"
+                type="password"
+                value={password}
               />
             </div>
-           
-            <p className="text-neutral-500 mt-8 mb-8">
-              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
+
+            <button
+              onClick={variant === "login" ? "login" : "register"}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center"></div>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
               .
             </p>
