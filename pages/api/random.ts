51a42b8dbe225450228937ae44a94696a50f5224
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-09-21 16:16:57 -0600

    feat: add Billboard component

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -1,28 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from '@/libs/prismadb';
+import serverAuth from "@/libs/serverAuth";
 
-
-import { NextApiRequest, NextApiResponse} from "next";
-import prismadb  from '@/libs/prismadb';
-import serverAuth  from '@/libs/serverAuth';
-
-export default async function handle(req:NextApiRequest, res: NextApiResponse) {
-    if(req.method === 'GET'){
-        return res.status(405).end();
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
     }
-    try{
-        await serverAuth(req, res);
 
-        const movieCount = await prismadb.movie.count();
-        const randomIndex = Math.floor(Math.random() * movieCount);
+    await serverAuth(req, res);
 
-        const randomMovies = await prismadb.movie.findMany({
-            take: 1,
-            skip: randomIndex
-        });
+    const moviesCount = await prismadb.movie.count();
+    const randomIndex = Math.floor(Math.random() * moviesCount);
 
-        return res.status(200).json(randomMovies[0]);
+    const randomMovies = await prismadb.movie.findMany({
+      take: 1,
+      skip: randomIndex
+    });
 
-    } catch(error){
-        console.log(error);
-        return res.status(400).end();
-    }
+    return res.status(200).json(randomMovies[0]);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).end();
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -27,6 +28,7 @@ export default function Home() {
   return (
     <>
       <Navbar/>
+      <Billboard/>
     </>
   );
 }
