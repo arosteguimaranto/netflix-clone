f30774447b0a3ef4b22c3c90fa4275a69fa4db27
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-09-25 12:09:51 -0600

    feat: add MovieList and MovieCard components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,15 @@ import { getSession } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
-  if(!session){
-    return{ 
+  if (!session) {
+    return {
       redirect: {
         destination: '/auth',
         permanent: false,
@@ -17,18 +19,22 @@ export async function getServerSideProps(context: NextPageContext) {
     }
   }
 
-  return{
-    props:{}
+  return {
+    props: {}
   }
 }
 
 export default function Home() {
-  const {data: user } = useCurrentUser();
+  const { data: movies } = useMovieList();
 
   return (
     <>
-      <Navbar/>
-      <Billboard/>
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
+
     </>
   );
 }
